6b41ab1ff8b00dd166c1fdc6e4dfddc8245d247e
Arutyunyan Artyom <artalar@ya.ru>
2022-02-10 09:36:32 +0000

    add invert and small fixes for sandbox

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ import "./mono.css";
 
 extend([a11yPlugin]);
 
+toDefault();
+function toDefault() {
+  document.body.style.setProperty("--mono-main", "#000000");
+  document.body.style.setProperty("--mono-back", "#ffffff");
+  document.getElementById("colorpicker").value = "#000000";
+}
+
 document.getElementById("colorpicker").addEventListener("input", (event) => {
   const main = event.target.value;
   const back = colord(main).invert().toHex();
@@ -12,8 +19,14 @@ document.getElementById("colorpicker").addEventListener("input", (event) => {
   document.body.style.setProperty("--mono-main", main);
   document.body.style.setProperty("--mono-back", back);
 });
-document.getElementById("colorreset").addEventListener("click", (event) => {
-  document.body.style.setProperty("--mono-main", "black");
-  document.body.style.setProperty("--mono-back", "white");
-  document.getElementById("colorpicker").value = "black";
+document.getElementById("colorreset").addEventListener("click", toDefault);
+
+document.getElementById("colorinvert").addEventListener("click", (event) => {
+  console.log(document.body.style);
+  const back = document.body.style.getPropertyValue("--mono-main");
+  const main = colord(back).invert().toHex();
+
+  document.body.style.setProperty("--mono-main", main);
+  document.body.style.setProperty("--mono-back", back);
+  document.getElementById("colorpicker").value = main;
 });
