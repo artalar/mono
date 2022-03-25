86e0d745a253bf3769856b28bbaf173f902db2aa
Arutyunyan Artyom <artalar@ya.ru>
2022-03-25 19:46:20 +0000

    fix sandbox hmr for gitpod

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+
+let hmr = true;
+let port = 3000;
+
+if (process.env.GITPOD_WORKSPACE_CONTEXT) {
+  hmr = {
+    host: `${port}-${process.env.GITPOD_WORKSPACE_ID}.${process.env.GITPOD_WORKSPACE_CLUSTER_HOST}`,
+    port: 443,
+    protocol: "wss",
+  };
+}
 
 export default defineConfig({
-  server: {
-    hmr: true
-  }
-})
+  server: { hmr, port },
+});
