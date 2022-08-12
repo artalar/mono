dd417206b00fc2dd6f2ace20d815fd4381be4a79
artalar <artalar@ya.ru>
2022-08-12 22:51:34 +0000

    add prefers-color-scheme matching

--- main.js
+++ main.js
@@ -5,6 +5,41 @@ import "./mono.css";
 
 extend([a11yPlugin]);
 
+const DEFAULT_MAIN = "#111111";
+const DEFAULT_BACK = "#dddddd";
+
+const MEDIA_DARK = "(prefers-color-scheme: dark)";
+const MEDIA_LIGHT = "(prefers-color-scheme: light)";
+
+detectColorScheme();
+function detectColorScheme() {
+  if (!window.matchMedia) {
+    return;
+  }
+  function listener({ matches, media }) {
+    if (!matches) {
+      // Not matching anymore = not interesting
+      return;
+    }
+    if (media === MEDIA_DARK) {
+      changeWebsiteTheme(DEFAULT_BACK, DEFAULT_MAIN);
+    } else if (media === MEDIA_LIGHT) {
+      changeWebsiteTheme(DEFAULT_MAIN, DEFAULT_BACK);
+    }
+  }
+  const mqDark = window.matchMedia(MEDIA_DARK);
+  mqDark.addEventListener("change", listener);
+  const mqLight = window.matchMedia(MEDIA_LIGHT);
+  mqLight.addEventListener("change", listener);
+}
+
+function changeWebsiteTheme(main, back) {
+  document.body.style.setProperty("--mono-main", main);
+  document.body.style.setProperty("--mono-back", back);
+  pickerMain.value = main;
+  pickerBack.value = back;
+}
+
 let isSync = true;
 
 const pickerMain = document.getElementById("colorpicker-main");
@@ -43,29 +78,22 @@ document.getElementById("colormix").addEventListener("click", (event) => {
   );
   const back = contrast(main);
 
-  document.body.style.setProperty("--mono-main", main.toHex());
-  document.body.style.setProperty("--mono-back", back.toHex());
-  pickerMain.value = main.toHex();
-  pickerBack.value = back.toHex();
+  changeWebsiteTheme(main.toHex(), back.toHex());
 });
 
 document.getElementById("colorinvert").addEventListener("click", (event) => {
   const back = document.body.style.getPropertyValue("--mono-main");
   const main = document.body.style.getPropertyValue("--mono-back");
 
-  document.body.style.setProperty("--mono-main", main);
-  document.body.style.setProperty("--mono-back", back);
-  pickerMain.value = main;
-  pickerBack.value = back;
+  changeWebsiteTheme(main, back);
 });
 
 document.getElementById("colorreset").addEventListener("click", toDefault);
 
 function toDefault() {
-  document.body.style.setProperty("--mono-main", "#111111");
-  document.body.style.setProperty("--mono-back", "#dddddd");
-  pickerMain.value = "#111111";
-  pickerBack.value = "#dddddd";
+  window.matchMedia(MEDIA_DARK).matches
+    ? changeWebsiteTheme(DEFAULT_BACK, DEFAULT_MAIN)
+    : changeWebsiteTheme(DEFAULT_MAIN, DEFAULT_BACK);
 }
 
 function contrast(color) {
